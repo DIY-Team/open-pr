ee9fe0f6acf4f836ff6113010e379469d7760d8b
v-i-s-h-a-l <vishalsingh2706@gmail.com>
2020-04-22 07:28:34 +0530

    update readme with installation instructions

--- Sources/OpenPR/main.swift
+++ Sources/OpenPR/main.swift
@@ -4,15 +4,6 @@ let arguments = CommandLine.arguments
 
 // execute installation if the command arguments contain --install
 if arguments.count > 1, arguments[1] == "--install" {
-//    var installationArguments = [arguments.first!]
-//
-//    if arguments.count > 2 {
-//        // tool name is provided by dev
-//        let toolName = arguments[2]
-//        installationArguments.append(toolName)
-//    } else {
-//
-//    }
     let tool = InstallOpenPR(arguments: arguments)
     do {
         try tool.run()
@@ -21,7 +12,6 @@ if arguments.count > 1, arguments[1] == "--install" {
     }
 } else {
     // Execute core functionaliy
-    
     let tool = OpenPR()
     do {
         try tool.run()
