0ef51c7b35661190f3e2011a394aadd0774ff911
Vishal Singh <vishalsingh2706@gmail.com>
2020-04-22 06:17:37 +0530

    update read me and refactor package.swift

--- Package.swift
+++ Package.swift
@@ -6,16 +6,12 @@ import PackageDescription
 let package = Package(
     name: "OpenPR",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "OpenPRCore"),
         .target(
             name: "OpenPR",
-            dependencies: ["OpenPRCore"]),
+            dependencies: ["OpenPRCore"])
     ]
 )
